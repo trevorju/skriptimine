cdfa3235b2c125bbbcd2f4155c0b6207279f90d8
Trevor Juurma <trevor.juurma@voco.com>
2024-02-02 15:30:56 +0200

    php7.4

--- php_paigaldamine.sh
+++ php_paigaldamine.sh
@@ -1,16 +1,16 @@
 # php paigaldusskript
 #
-#kontrollime, mitu korda php korral ok installed
+#kontrollime, mitu korda php7.4 korral ok installed
 # sõnad on leitud ja vastus salvestame
 # muutuja sisse:
-PHP=$ (dpkg-querty -W -f='${Status}' php 2>/dev/null | grep -c 'ok installed' )
+PHP=$ (dpkg-querty -W -f='${Status}' php7.4 2>/dev/null | grep -c 'ok installed' )
 # kui PHP muutuja väärtus võrdub 0-ga
 if [ $PHP -eq 0 ]; then
 	# siis ok installed ei ole leitud
 	# ja väljastame vastava teate
 	# paigaldame teenuse
 	echo "Paigaldame php ja vajalikud lisad"
-	apt install php libapache2-mod-php php mysql
+	apt install php7.4 libapache2-mod-php php7.4-mysql
 	echo "php on paigaldatud"
 # kui PHP muutuja väärtus võrdub 1-ga
 elif [ $PHP -eq 1 ]; then
